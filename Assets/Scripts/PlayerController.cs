b39424bf955ee9b8ab47b1b32b9f671a342f72e8
someguy13 <ryan.kwong@gmail.com>
2022-01-29 11:33:55 -0800

    Added new sprite animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float dist = 2f;
     public Transform movePoint;
+    private Animator anim;
 
     public LayerMask noMove;
     
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         movePoint.parent = null;
     }
 
@@ -24,19 +27,37 @@ public class PlayerController : MonoBehaviour
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f),
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal")*dist, 0f, 0f),
                     .2f, noMove))
                 {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    anim.SetBool("isMoving",true);
+                    anim.SetFloat("Horizontal",Input.GetAxisRaw("Horizontal"));
+                    anim.SetFloat("Vertical",0);
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal")*dist, 0f, 0f);
+                }
+                else
+                {
+                    anim.SetBool("isMoving",false);
                 }
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f,Input.GetAxisRaw("Vertical"), 0f),
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f,Input.GetAxisRaw("Vertical")*dist, 0f),
                     .2f, noMove))
                 {
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    anim.SetBool("isMoving",true);
+                    anim.SetFloat("Horizontal",0);
+                    anim.SetFloat("Vertical",Input.GetAxisRaw("Vertical"));
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical")*dist, 0f);
                 }
+                else
+                {
+                    anim.SetBool("isMoving",false);
+                }
+            }
+            else
+            {
+                anim.SetBool("isMoving",false);
             }
         }
 
